2f0d0f3fdd081e76d0ae4cfb2fb40241d439629a
Tianah10 <tobicris10@gmail.com>
2026-01-02 12:44:23 +0100

    Delete  Ass 1.js

---  Ass 1.js
+++
@@ -1,7 +0,0 @@
-// Welcome to JavaScript Code Pad
-// Runs on the embedded node.js
-
-  var myLastName = "Ojo";
-  var myFirstName = "Christianah";
-  var myAge = 22;
-  console.log("My name is " + myLastName + " " + myFirstName + " and i am " + myAge + "years old");
