a6ef8cac20d60d7c620503263d4652158b3c28b4
Tianah10 <tobicris10@gmail.com>
2026-01-02 12:45:07 +0100

    Delete Ass 4.js

--- Ass 4.js
+++
@@ -1,7 +0,0 @@
-// Welcome to JavaScript Code Pad
-// Runs on the embedded node.
-var Price = 50;
-// subtract discount from price
-let discount = Price * 0.10;
-let finalprice = Price - discount;
-console.log(finalprice);
